8a35432c5784f1f481dbf75478ea96050980241c
Justin Shaw <easyworkemail@gmail.com>
2019-04-30 18:40:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |key, value|
       puts "#{key.capitalize}:"
     value.each do |holiday, supplies|
-      puts "  #{holiday.capitalize}: "
+      print "  #{holiday.capitalize}: "
       supplies.each do |supply|
         puts "#{supply}"
       end #{supplies}"
