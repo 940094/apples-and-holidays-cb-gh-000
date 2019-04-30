eedeaf6ea3506dbb2c7ca7b0a0600be4e8410649
Justin Shaw <easyworkemail@gmail.com>
2019-04-30 18:33:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,6 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |seasons, holidays, suppliess|
-    puts seasons
     seasons.each do |holiday, supplies|
       puts "#{holiday}: #{supplies}"
     end
