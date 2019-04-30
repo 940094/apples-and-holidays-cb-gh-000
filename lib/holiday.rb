58c8ca50450d70fc65fbd1140da8ea4683d78d08
Justin Shaw <easyworkemail@gmail.com>
2019-04-30 18:15:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,7 +44,8 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # remember to return the updated hash
   # binding.pry
   puts season
-  puts holiday_hash[season]
+  p holiday_hash[season]
+  p holiday_hash[:season]
   puts holiday_hash[season][holiday_name]
   # holiday_hash[season][holiday_name] => supply_array
   # holiday_hash[:fall][fun_holiday] => ['supply1', 'supply2']
